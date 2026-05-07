df2ca9fe742baf7e9e99e5b2fe78832c10bfabb8
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-05-07 03:13:37 +0200

    added dialog and dialog html template

--- script.js
+++ script.js
@@ -17,7 +17,26 @@ function renderImages() {
     let div = document.getElementById('images');
     div.innerHTML = "";
     images.forEach(element => {
-        div.innerHTML += `<div class='image_card' onclick="openDialog(${element})" style='background-image: url("./assets/img/${element}")'></div>`;
-        console.log(div);
+        div.innerHTML += `<div class='image_card' onclick="openDialog(${images.indexOf(element)})" style='background-image: url("./assets/img/${element}")'></div>`;
     });
+}
+
+function openDialog(index) {
+    const dialogRef = document.getElementById('image_dialog');
+    dialogRef.innerHTML = `        
+    <div class="dialog_wrapper">
+            <header class="dialog_header">
+                <h2>${images[index]}</h2>
+                <button>X</button>
+            </header>
+            <section>
+                <img src="./assets/img/${images[index]}" alt="">
+            </section>
+            <div class="dialog_footer">
+                <button>Pfeil</button>
+                <p>1/12</p>
+                <button>Pfeil</button>
+            </div>
+        </div>`;
+    dialogRef.showModal();
 }
